6241f31c70937682a670c6420510d7de4cf6e8cf
Brian Busemeyer <bbusemeyer@gmail.com>
2016-02-06 22:03:17 -0600

    Add MnO.

---
+++ mno/run_tests.py
@@ -0,0 +1,53 @@
+import cif2crystal
+import runcrystal
+import runqwalk
+import copy
+import job_control as jc
+import os
+import json
+
+import veritas
+
+element_list=[]
+element_list.append(cif2crystal.Cif2Crystal())
+element_list.append(runcrystal.RunCrystal(
+  submitter=veritas.LocalVeritasCrystalSubmitter(
+    nn=1,np=8,time="0:20:00",queue="batch")))
+element_list.append(runcrystal.RunProperties(
+  submitter=veritas.LocalVeritasPropertiesSubmitter(
+    nn=1,np=1,time="1:00:00",queue="batch")))
+element_list.append(runqwalk.Crystal2QWalk())
+element_list.append(runqwalk.QWalkVarianceOptimize(
+  submitter=veritas.LocalVeritasQwalkSubmitter(
+    nn=1,np=8,time="0:10:00",queue="batch")))
+element_list.append(runqwalk.QWalkEnergyOptimize(
+  submitter=veritas.LocalVeritasQwalkSubmitter(
+    nn=1,np=8,time="0:10:00",queue="batch")))
+element_list.append(runqwalk.QWalkRunDMC(
+  submitter=veritas.LocalVeritasBundleQwalkSubmitter(
+    nn=1,np=8,time="0:10:00",queue="batch")))
+
+default_job=jc.default_job_record("MnO.cif")
+default_job['supercell'] = [[2,0,0],[0,2,0],[0,0,2]]
+default_job['total_spin'] = 0
+default_job['dft']['kmesh'] = [2,2,2]
+default_job['dft']['functional']['hybrid'] = 0
+default_job['dft']['tolinteg'] = [6,6,6,6,12]
+default_job['dft']['basis']=[0.2,2,2]
+default_job['dft']['maxcycle'] = 100
+default_job['dft']['fmixing'] = 80
+default_job['dft']['edifftol'] = 6
+default_job['dft']['broyden'] = [0.1,60,20]
+default_job['dft']['initial_spin'] = [1,-1,0,0]
+default_job['dft']['intial_charges'] = {"Mn":2,"O":-2}
+default_job['qmc']['variance_optimize']['reltol']=0.1
+default_job['qmc']['variance_optimize']['abstol']=10
+default_job['qmc']['dmc']['save_trace'] = False
+default_job['qmc']['dmc']['nblock']=5
+default_job['qmc']['dmc']['target_error']=0.1
+
+
+name = "referece"
+job_record = copy.deepcopy(default_job)
+job_record['control']['id']=name
+jc.execute(job_record,element_list)
